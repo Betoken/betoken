1bb7b094b01ad6865fffeb7065badfaf867e035e
Zefram Lou <zeframlou@gmail.com>
2017-11-10 20:59:04 -0800

    Updated

--- contracts/betoken.sol
+++ contracts/betoken.sol
@@ -10,15 +10,25 @@ contract GroupFund {
     bool isBuy;
     address tokenAddress;
     uint amount;
+    //Proportion of control people who vote against a proposal have to stake
+    uint againstStakeProp;
+    mapping(address => bool) userSupportsProposal;
   }
 
+  modifier isChangeMakingTime {
+    require(block.timestamp < startTimeOfCycle + timeOfChangeMaking);
+    _;
+  }
+
+  //Number of decimals used for decimal numbers
   uint decimals;
 
   // A list of everyone who is participating in the GroupFund
   address[] participants;
 
-  // The proportion a person owns of the totalFunds
-  mapping(address => uint) shares;
+  // Maps user address to their initial deposit
+  mapping(address => uint) initialDeposit;
+  uint totalInitialDeposit;
 
   //Address of the control token
   address controlTokenAddr;
@@ -28,14 +38,74 @@ contract GroupFund {
 
   Proposal[] proposals;
 
-  //Proportion of control people who vote against a proposal have to stake
-  uint againstStakeProp;
+  //The start time for the current investment cycle, in seconds since Unix epoch
+  uint startTimeOfCycle;
 
-  //Temporal length of each investment cycle
+  //Temporal length of each investment cycle, in seconds
   uint timeOfCycle;
 
-  function GroupFund(uint _decimals, uint _timeOfCycle) {
+  //Temporal length of change making period at start of each cycle, in seconds
+  uint timeOfChangeMaking;
+
+  function GroupFund(
+    address _controlTokenAddr,
+    uint _decimals,
+    uint _timeOfCycle,
+    uint _timeOfChangeMaking
+  )
+  {
+    controlTokenAddr = _controlTokenAddr;
     decimals = _decimals;
+    startTimeOfCycle = 0;
+    timeOfCycle = _timeOfCycle;
+    timeOfChangeMaking = _timeOfChangeMaking;
+  }
+
+  function startNewCycle() {
+
+  }
+
+  function createProposal()
+    isChangeMakingTime
+  {
+
+  }
+
+  function supportProposal()
+    isChangeMakingTime
+  {
+
+  }
+
+  function deposit()
+    isChangeMakingTime
+  {
+
+  }
+
+  function withdraw()
+    isChangeMakingTime
+  {
+
+  }
+
+  function endChangeMakingTime() {
+    require(block.timestamp >= startTimeOfCycle + timeOfChangeMaking);
+    require(block.timestamp < startTimeOfCycle + timeOfCycle);
+
+  }
+
+  function calculateAgainstStakeProp(uint proposalId) view {
+    uint numFor = 0;
+    uint numAgainst = 0;
+    for (uint i = 0; i < participants.length; i++) {
+      bool isFor = proposals[proposalId].userSupportsProposal[participants[i]];
+      if (isFor) {
+        numFor++;
+      }
+    }
+    numAgainst = participants.length - numFor;
+    //Todo: use control tokens to calculate againstStakeProp
   }
 
   function() {
