657b3236afb02324c76594ce8183529d95d4bbfd
Zefram Lou <zeframlou@gmail.com>
2017-11-11 12:14:25 -0800

    Receipients of control token are automatically part of the group fund.

--- contracts/betoken.sol
+++ contracts/betoken.sol
@@ -31,44 +31,48 @@ contract GroupFund {
   }
 
   //Number of decimals used for decimal numbers
-  uint256 decimals;
+  uint256 public decimals;
 
   // A list of everyone who is participating in the GroupFund
-  address[] participants;
-  mapping(address => bool) isParticipant;
+  address[] public participants;
+  mapping(address => bool) public isParticipant;
 
   // Maps user address to their initial deposit
-  mapping(address => uint256) initialDeposit;
-  uint256 totalInitialDeposit;
+  mapping(address => uint256) public initialDeposit;
+  uint256 public totalInitialDeposit;
 
   //Address of the control token
-  address controlTokenAddr;
+  address public controlTokenAddr;
 
   // The total amount of funds held by the group
-  uint256 totalFundsInWeis;
+  uint256 public totalFundsInWeis;
+
+  uint256 public totalFundsAtStartOfCycleInWeis;
 
   //The start time for the current investment cycle, in seconds since Unix epoch
-  uint256 startTimeOfCycle;
+  uint256 public startTimeOfCycle;
 
   //Temporal length of each investment cycle, in seconds
-  uint256 timeOfCycle;
+  uint256 public timeOfCycle;
 
   //Temporal length of change making period at start of each cycle, in seconds
-  uint256 timeOfChangeMaking;
+  uint256 public timeOfChangeMaking;
 
   //Indicates whether the cycle has started and is not past ending time
-  bool cycleIsActive;
+  bool public cycleIsActive;
 
-  bool changeMakingTimeHasEnded;
+  bool public changeMakingTimeHasEnded;
 
-  mapping(address => uint256) balanceOf;
+  bool public isFirstCycle;
 
-  mapping(uint256 => uint256) stakedControlOfProposal;
+  mapping(address => uint256) public balanceOf;
 
-  mapping(uint256 => mapping(address => uint256)) stakedControlOfUserOfProposal;
+  mapping(uint256 => uint256) public stakedControlOfProposal;
 
-  Proposal[] proposals;
-  ControlToken cToken;
+  mapping(uint256 => mapping(address => uint256)) public stakedControlOfProposalOfUser;
+
+  Proposal[] public proposals;
+  ControlToken public cToken;
 
   event CycleStarted(uint256 timestamp);
   event ChangeMakingTimeEnded(uint256 timestamp);
@@ -79,18 +83,20 @@ contract GroupFund {
     uint256 _timeOfCycle,
     uint256 _timeOfChangeMaking
   )
+    public
   {
     decimals = _decimals;
     startTimeOfCycle = 0;
     timeOfCycle = _timeOfCycle;
     timeOfChangeMaking = _timeOfChangeMaking;
+    isFirstCycle = true;
 
     //Create control token contract
     cToken = new ControlToken();
     controlTokenAddr = cToken;
   }
 
-  function startNewCycle() {
+  function startNewCycle() public {
     require(!cycleIsActive);
     require(now >= startTimeOfCycle.add(timeOfCycle));
 
@@ -106,10 +112,11 @@ contract GroupFund {
     address _tokenAddress,
     uint256 _amount
   )
+    public
     isChangeMakingTime
     onlyParticipant
   {
-    //require(amount <= controlOf(msg.sender));
+    //require(_amount <= cToken.balanceOf(msg.sender));
     proposals.push({
       isBuy: _isBuy,
       tokenAddress: _tokenAddress,
@@ -119,6 +126,7 @@ contract GroupFund {
   }
 
   function supportProposal(uint256 proposalId, uint256 controlStake)
+    public
     isChangeMakingTime
     onlyParticipant
   {
@@ -126,13 +134,14 @@ contract GroupFund {
 
     //Stake control tokens
     stakedControlOfProposal[proposalId] = stakedControlOfProposal[proposalId].add(controlStake);
-    stakedControlOfUserOfProposal[proposalId][msg.sender] = stakedControlOfUserOfProposal[proposalId][msg.sender].add(controlStake);
+    stakedControlOfProposalOfUser[proposalId][msg.sender] = stakedControlOfProposalOfUser[proposalId][msg.sender].add(controlStake);
     cToken.ownerCollectFrom(msg.sender, controlStake);
 
     //Make investment
   }
 
   function deposit()
+    public
     payable
     isChangeMakingTime
   {
@@ -146,14 +155,18 @@ contract GroupFund {
     totalInitialDeposit = totalInitialDeposit.add(msg.value);
     balanceOf[msg.sender] = balanceOf[msg.sender].add(msg.value);
 
-    //Give control tokens proportional to investment
-    cToken.mint(msg.sender, msg.value);
+    if (isFirstCycle) {
+      //Give control tokens proportional to investment
+      cToken.mint(msg.sender, msg.value);
+    }
   }
 
   function withdraw(uint256 amountInWeis)
+    public
     isChangeMakingTime
     onlyParticipant
   {
+    require(!isFirstCycle);
     require(msg.sender.balance + amount >= msg.sender.balance);
 
     balanceOf[msg.sender] = balanceOf[msg.sender].sub(amountInWeis);
@@ -168,24 +181,40 @@ contract GroupFund {
 
     changeMakingTimeHasEnded = true;
 
-    //Do stuff
+    //Stake against votes
 
     ChangeMakingTimeEnded(now);
   }
 
-  function endCycle() {
+  function endCycle() public {
     require(cycleIsActive);
     require(now >= startTimeOfCycle.add(timeOfCycle));
 
     cycleIsActive = false;
+    isFirstCycle = false;
 
     //Distribute staked control tokens
 
     //Sell all invested tokens
 
+    //totalFundsInWeis = this.balance;
+    //totalFundsAtStartOfCycleInWeis = this.balance;
+
+    //Distribute ether balance
+
+    //Distribute commission
+  }
+
+  function addControlTokenReceipientAsParticipant(address receipient) public {
+    require(msg.sender == controlTokenAddr);
+    if (!isParticipant[receipient]) {
+      isParticipant[receipient] = true;
+      participants.push(receipient);
+    }
   }
 
   function calculateAgainstStakeProp(uint256 proposalId)
+    public
     view
     returns(uint256 againstStakeProp)
   {
@@ -201,16 +230,18 @@ contract GroupFund {
       if (isFor) {
         numFor = numFor.add(1);
       } else {
-        againstTotalControl = againstTotalControl.add(cToken.balanceOf(participant));
+        if (cToken.balanceOf(participant) != 0) {
+          numAgainst = numAgainst.add(1);
+          againstTotalControl = againstTotalControl.add(cToken.balanceOf(participant));
+        }
       }
     }
     forStakedControl = stakedControlOfProposal[proposalId];
-    numAgainst = participants.length.sub(numFor);
 
     return numFor.mul(forStakedControl).mul(10**decimals).div(numAgainst.mul(againstTotalControl));
   }
 
-  function() {
+  function() public {
     revert();
   }
 }
@@ -219,12 +250,36 @@ contract GroupFund {
 contract ControlToken is MintableToken {
   using SafeMath for uint256;
 
-  function ownerCollectFrom(address _from, uint256 _value) public onlyOwner {
+  mapping(address => bool) hasOwnedTokens;
+
+  event OwnerCollectFrom(address _from, uint256 value);
+
+  function transfer(address _to, uint256 _value) public returns(bool) {
+    require(_to != address(0));
+    require(_value <= balances[msg.sender]);
+
+    //Add receipient as a participant if not already a participant
+    if (!hasOwnedTokens[_to]) {
+      hasOwnedTokens[_to] = true;
+      GroupFund g = GroupFund(owner);
+      g.addControlTokenReceipientAsParticipant(_to);
+    }
+
+    // SafeMath.sub will throw if there is not enough balance.
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function ownerCollectFrom(address _from, uint256 _value) public onlyOwner returns(bool) {
     require(_from != address(0));
     require(_value <= balances[_from]);
 
     // SafeMath.sub will throw if there is not enough balance.
     balances[_from] = balances[_from].sub(_value);
     balances[msg.sender] = balances[msg.sender].add(_value);
+    OwnerCollectFrom(_from, _value);
+    return true;
   }
 }
