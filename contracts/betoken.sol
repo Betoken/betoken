3e4a967dc72fd21234dc4176cd4e55e841ede0eb
Zefram Lou <zeframlou@gmail.com>
2017-11-10 21:49:03 -0800

    Installed Open Zeppelin, made changes

--- contracts/betoken.sol
+++ contracts/betoken.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.18;
 
+import 'zeppelin-solidity/contracts/token/StandardToken.sol';
+
 // The main contract that keeps track of:
 // - Who is in the fund
 // - How much the fund has
@@ -25,6 +27,7 @@ contract GroupFund {
 
   // A list of everyone who is participating in the GroupFund
   address[] participants;
+  mapping(address => bool) isParticipant;
 
   // Maps user address to their initial deposit
   mapping(address => uint) initialDeposit;
@@ -36,8 +39,6 @@ contract GroupFund {
   // The total amount of funds held by the group
   uint totalFundsInWeis;
 
-  Proposal[] proposals;
-
   //The start time for the current investment cycle, in seconds since Unix epoch
   uint startTimeOfCycle;
 
@@ -46,6 +47,14 @@ contract GroupFund {
 
   //Temporal length of change making period at start of each cycle, in seconds
   uint timeOfChangeMaking;
+
+  bool hasStarted;
+  bool hadEnded;
+
+  Proposal[] proposals;
+
+  event StartedNewCycle(uint timestamp);
+  event CycleEnded(uint timestamp);
 
   function GroupFund(
     address _controlTokenAddr,
@@ -62,13 +71,30 @@ contract GroupFund {
   }
 
   function startNewCycle() {
+    require(!hasStarted && hasEnded);
+    require(block.timestamp >= startTimeOfCycle + timeOfCycle);
 
+    hasStarted = true;
+    hasEnded = false;
+
+    startTimeOfCycle = block.timestamp;
+    StartedNewCycle(block.timestamp);
   }
 
-  function createProposal()
+  function createProposal(
+    bool _isBuy,
+    address _tokenAddress,
+    uint _amount
+  )
     isChangeMakingTime
   {
-
+    //require(amount <= controlOf(msg.sender));
+    proposals.push({
+      isBuy: _isBuy,
+      tokenAddress: _tokenAddress,
+      amount: _amount,
+      againstStakeProp: calculateAgainstStakeProp()
+    });
   }
 
   function supportProposal()
@@ -78,12 +104,12 @@ contract GroupFund {
   }
 
   function deposit()
+    payable
     isChangeMakingTime
   {
 
   }
 
-<<<<<<< HEAD
   function withdraw()
     isChangeMakingTime
   {
@@ -91,9 +117,14 @@ contract GroupFund {
   }
 
   function endChangeMakingTime() {
+    require(hasStarted && !hasEnded);
     require(block.timestamp >= startTimeOfCycle + timeOfChangeMaking);
     require(block.timestamp < startTimeOfCycle + timeOfCycle);
 
+    hasStarted = false;
+    hasEnded = true;
+
+    CycleEnded(block.timestamp);
   }
 
   function calculateAgainstStakeProp(uint proposalId) view {
@@ -113,11 +144,3 @@ contract GroupFund {
     revert();
   }
 }
-=======
-  function() {
-    revert();
-  }
-}
-
-// Make a ShareToken based on the ERC20 Boilerplate
->>>>>>> 91cc670eae8b318f8491494a18d30ac60a3d7122
