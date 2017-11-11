3cdfcfe21718e2a19aa213a99699babdbdd35859
Zefram Lou <zeframlou@gmail.com>
2017-11-10 16:58:11 -0800

    Updated betoken.sol

--- contracts/betoken.sol
+++ contracts/betoken.sol
@@ -6,12 +6,39 @@ pragma solidity ^0.4.18;
 // - Each person's Share
 // - Each person's Control
 contract GroupFund {
+  struct Proposal {
+    bool isBuy;
+    address tokenAddress;
+    uint amount;
+  }
+
+  uint decimals;
+
   // A list of everyone who is participating in the GroupFund
   address[] participants;
 
   // The proportion a person owns of the totalFunds
   mapping(address => uint) shares;
 
+  //Address of the control token
+  address controlTokenAddr;
+
   // The total amount of funds held by the group
-  uint totalFunds;
+  uint totalFundsInWeis;
+
+  Proposal[] proposals;
+
+  //Proportion of control people who vote against a proposal have to stake
+  uint againstStakeProp;
+
+  //Temporal length of each investment cycle
+  uint timeOfCycle;
+
+  function GroupFund(uint _decimals, uint _timeOfCycle) {
+    decimals = _decimals;
+  }
+
+  function() {
+    revert();
+  }
 }
