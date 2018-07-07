5deb0ce0b273e26545f66bb8aab0854b33a3efb2
Zefram Lou <zeframlou@gmail.com>
2018-07-07 13:47:30 -0400

    updated travis

--- eth/truffle-config.js
+++ eth/truffle-config.js
@@ -7,7 +7,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      gas: 8000000,
+      gas: 6000000,
       gasPrice: Math.pow(10, 8),
       network_id: "*" // match any network
     },
