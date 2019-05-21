8a5fdbf018c91389b8441a36772a778853295a3d
Zefram Lou <zeframlou@gmail.com>
2019-05-20 18:36:30 -0700

    fixed 18. "no need for storing betokenFund"

--- eth/contracts/BetokenProxy.sol
+++ eth/contracts/BetokenProxy.sol
@@ -3,19 +3,16 @@ pragma solidity 0.5.0;
 import "./BetokenFund.sol";
 
 contract BetokenProxy {
-  address public betokenFundAddress;
-  BetokenFund internal betokenFund;
+  address payable public betokenFundAddress;
 
   constructor(address payable _fundAddr) public {
     betokenFundAddress = _fundAddr;
-    betokenFund = BetokenFund(_fundAddr);
   }
 
   function updateBetokenFundAddress() public {
     require(msg.sender == betokenFundAddress, "Sender not BetokenFund");
-    address payable nextVersion = betokenFund.nextVersion();
+    address payable nextVersion = BetokenFund(betokenFundAddress).nextVersion();
     require(nextVersion != address(0), "Next version can't be empty");
     betokenFundAddress = nextVersion;
-    betokenFund = BetokenFund(nextVersion);
   }
 }
